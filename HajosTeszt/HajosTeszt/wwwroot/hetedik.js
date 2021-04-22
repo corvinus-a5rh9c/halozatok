2fc69b05594173fbe4af2af6935cf952f5ae68cb
Krisztián Tolnai <krisztian.tolnai@stud.uni-corvinus.hu>
2021-04-22 18:19:22 +0200

    REST API

--- HajosTeszt/HajosTeszt/wwwroot/hetedik.js
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -1,9 +1,19 @@
 window.onload = () => {
-    letoltes();
+    //letoltes();
+    fetch('/questions/1')
+        .then(response => response.json())
+        .then(data => kerdesMegjelenites(data)
+        );
 }
 
 var kerdesek;
 var sorszam = 0;
+var kerdesszam
+
+var valasz1
+var valasz2
+var valasz3
+
 function letoltes() {
     fetch("/questions.json")
         .then(response => response.json())
@@ -17,42 +27,64 @@ function letoltesBefejezodott(n) {
 
 }
 
-var kerdesmegjelenites = function(kerdesszam) {
-    let kerdesSzoveg = document.getElementById("kerdes");
-    let kep = document.getElementById("kep1");
-    let valasz1 = document.getElementById("valasz1");
-    let valasz2 = document.getElementById("valasz2");
-    let valasz3 = document.getElementById("valasz3");
 
-    kerdesSzoveg.innerHTML = kerdesek[kerdesszam].questionText
-    kep.src = "https://szoft1.comeback.hu/hajo/" + kerdesek[kerdesszam].image
-    valasz1.innerText = kerdesek[kerdesszam].answer1
-    valasz2.innerText = kerdesek[kerdesszam].answer2
-    valasz3.innerText = kerdesek[kerdesszam].answer3
-
-}
-
-function vissza() {
-    if (sorszam == 0) {
-        sorszam = kerdesek.length - 1;
-        letoltes();
+function kerdesMegjelenites(kerdes) {
+    console.log(kerdes);
+    document.getElementById("kerdes").innerText = kerdes.questionText
+    valasz1 = document.getElementById("valasz1").innerHTML = kerdes.answer1
+    valasz2 = document.getElementById("valasz2").innerHTML = kerdes.answer2
+    valasz3 = document.getElementById("valasz3").innerHTML = kerdes.answer3
+    if (kerdes.image !== "") {
+        document.getElementById("kep1").src = "https://szoft1.comeback.hu/hajo/" + kerdes.image;
     }
     else {
-        sorszam--;
-        letoltes();
+        document.getElementById("kep1").src =""
     }
+    correctAnswer = kerdes.correctAnswer
+    questionId = kerdes.questionId
+}
+
+function kerdesBetoltes(id) {
+    fetch(`/questions/${id}`)
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Hibás válasz: ${response.status}`)
+            }
+            else {
+                return response.json()
+            }
+        })
+        .then(data => kerdesMegjelenites(data));
+}   
+
+function vissza() {
+    //if (sorszam == 0) {
+    //    sorszam = kerdesek.length - 1;
+    //    letoltes();
+    //}
+    //else {
+    //    sorszam--;
+    //    letoltes();
+    //}
+    kerdesBetoltes(questionId - 1)
     clear();
 }
 
 function elore() {
-    if (sorszam == kerdesek.length-1) {
-        sorszam = 0;
-        letoltes();
+    //if (sorszam == kerdesek.length - 1) {
+    //    sorszam = 0;
+    //    letoltes();
+    //}
+    //else {
+    //    sorszam++;
+    //    letoltes();
+    //}
+    if (questionId == kerdesszam) {
+        kerdesBetoltes(1)
     }
     else {
-        sorszam++;
-        letoltes();
-    }
+        kerdesBetoltes(questionId + 1)
+    }  
     clear();
 }
 function clear() {
@@ -65,13 +97,15 @@ function clear() {
 }
 
 
+
+
 function eredmeny() {
-    if (kerdesek[sorszam].correctAnswer == "1") {
+    if (correctAnswer == "1") {
         document.getElementById("valasz1").classList.add("jo");
         document.getElementById("valasz2").classList.add("rossz");
         document.getElementById("valasz3").classList.add("rossz");
     }
-    else if (kerdesek[sorszam].correctAnswer == "2") {
+    else if (correctAnswer == "2") {
         document.getElementById("valasz2").classList.add("jo");
         document.getElementById("valasz1").classList.add("rossz");
         document.getElementById("valasz3").classList.add("rossz");
@@ -82,5 +116,5 @@ function eredmeny() {
         document.getElementById("valasz1").classList.add("rossz");
 
     }
-    
+
 }
